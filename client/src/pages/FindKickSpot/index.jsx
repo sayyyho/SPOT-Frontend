f96709199cd54c67c4f0a2377dc458025140b7e5
lwittyl <mercury0502@dgu.ac.kr>
2024-05-06 23:31:48 +0900

    주차장 찾기 페이지

--- client/src/pages/FindKickSpot/index.jsx
+++ client/src/pages/FindKickSpot/index.jsx
@@ -1,14 +1,21 @@
+import * as S from "./FindKickSpot.styled";
 import Parking from "../../components/Parking";
 import ParkText from "../../components/ParkText";
-import * as S from "./FindKickSpot.styled";
+import Circle from "../../components/Circle";
+import stepimage from "../../assets/images/step1.png";
+import stepimage2 from "../../assets/images/step2.png";
 
 function FindKickSpot() {
   return (
     <S.Container>
         <ParkText></ParkText>
         <Parking></Parking>
+        <S.CircleContainer>
+            <Circle image={stepimage} />
+            <Circle image={stepimage2} />
+        </S.CircleContainer>
     </S.Container>
   );
 }
-
 export default FindKickSpot;
+
