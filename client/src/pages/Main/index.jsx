02958311d06f3aba9a79cdc9403ecb590f6a1881
sayyyho <323psh@naver.com>
2024-05-06 22:01:31 +0900

    Feat: 버튼 컴포넌트 구현

--- client/src/pages/Main/index.jsx
+++ client/src/pages/Main/index.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import {Button} from '@/components/Button';
 
 export const Main = () => {
-  React.useEffect(() => {
-    console.log(document.cookie);
-  }, []);
   return (
     <div>
-      <div>this is main</div>
+      <Button
+        width="90%"
+        height="40%"
+        radius="10px 10px 10px 10px"
+        background="gray"
+      ></Button>
     </div>
   );
 };
