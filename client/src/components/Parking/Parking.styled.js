ad459660985c7934a385412a006895582e5055a3
lwittyl <mercury0502@dgu.ac.kr>
2024-05-06 22:15:24 +0900

    Feat: 주차장 찾기 구현

---
+++ client/src/components/Parking/Parking.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 78%;
+  height: 48vh;
+`;
+
+export const StyledImage = styled.img`
+  width: 100%;
+  height: 100%;
+`;
