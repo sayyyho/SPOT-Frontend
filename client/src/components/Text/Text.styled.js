10bc3ec743e40f5680646ca05758ae6e88f65c3a
sayyyho <323psh@naver.com>
2024-05-06 20:51:31 +0900

    Design: 온보딩(로그인) 페이지 UI 개선

--- client/src/components/Text/Text.styled.js
+++ client/src/components/Text/Text.styled.js
@@ -1,6 +1,17 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 80%;
   height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const TextArea = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 `;
--- client/src/components/Text/index.jsx
+++ client/src/components/Text/index.jsx
@@ -1,10 +1,13 @@
-import * as S from "./Text.styled";
+import * as S from './Text.styled';
 
 function Text() {
   return (
     <S.Container>
-      <h3>내 주변 킥보드 주차장 찾기</h3>
-      <div>올바른 공유 킥보드 주차 문화 만들기<br />spot에서 함께 시작하세요</div>
+      <S.TextArea>
+        <h2>내 주변 킥보드 주차장 찾기</h2>
+        <S.TextArea>올바른 공유 킥보드 주차 문화 만들기</S.TextArea>
+        SPOT에서 함께 시작하세요!
+      </S.TextArea>
     </S.Container>
   );
 }
