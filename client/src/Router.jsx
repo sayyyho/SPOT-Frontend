83b49ee2ca38becf405e17efaaff35152e2cc22c
JeongHeumChoi <andyc707@gmail.com>
2024-05-08 16:45:51 +0900

    Feat: 탑승 이력 조회 페이지 구현

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -1,10 +1,12 @@
 import {Route, Routes} from 'react-router-dom';
 import OnBoarding from './pages/OnBoarding';
+import History from "@/pages/History/index.jsx";
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<OnBoarding />} />
+      <Route path="/history" element={<History />} />
     </Routes>
   );
 }
---
+++ client/src/apis/getBoardHistoryList.js
@@ -0,0 +1,14 @@
+import {isAxiosError} from "axios";
+import {instance} from "./axios";
+
+export const getBoardHistoryList = async () => {
+  try {
+    return await instance.get(`boarding-record`);
+  } catch (error) {
+    if (isAxiosError(error)) {
+      throw error;
+    } else {
+      throw error;
+    }
+  }
+};
