d872a16cba2c4b2d5086428510aa355f37a6a312
sayyyho <323psh@naver.com>
2024-05-10 16:41:07 +0900

    Feat: 사진 업로드 기능 구현

---
+++ client/src/apis/spotUpload.js
@@ -0,0 +1,24 @@
+import {isAxiosError} from 'axios';
+import {instance} from './axios';
+
+export const spotUpload = async (imageFile) => {
+  try {
+    const data = {
+      distance: 1.2,
+      time: 10,
+    };
+
+    const formData = new FormData();
+    formData.append('image', imageFile);
+    formData.append('photoRequest', JSON.stringify(data));
+
+    const response = await instance.post(`/api/boarding-record`, formData);
+    return response;
+  } catch (error) {
+    if (isAxiosError(error)) {
+      throw error;
+    } else {
+      throw error;
+    }
+  }
+};
--- client/src/pages/FinishSpot/index.jsx
+++ client/src/pages/FinishSpot/index.jsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
-import * as S from "./FinishSpot.styled";
-import Parking from "../../components/Parking";
+import {useState} from 'react';
+import * as S from './FinishSpot.styled';
+import Parking from '../../components/Parking';
 import Layout from '@/components/Common/Layout/Layout';
-import Find_1 from "@/components/Common/Find_1";
-import Box from "@/components/Box";
+import Box from '@/components/Box';
+import Find_1 from '@/components/Common/Find_1';
 
 const FindKickSpot = () => {
   const [showParking, setShowParking] = useState(false);
@@ -23,20 +23,23 @@ const FindKickSpot = () => {
     borderRadius: '20px',
     padding: '1rem',
     boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
-  };  
+  };
 
   return (
     <Layout showHeader={true} headerItem="arrow">
       <S.Container>
         <h2>주차 인증하기</h2>
-        {showParking ? (<S.AdditionalText>주차 사진 업로드</S.AdditionalText>) : ( <Box margin="18px" /> )}
+        {showParking ? (
+          <S.AdditionalText>주차 사진 업로드</S.AdditionalText>
+        ) : (
+          <Box margin="18px" />
+        )}
         <Box style={greyBoxStyle}>
           {showParking ? (
             <>
-            {step === 1 && <Find_1 onNextClick={handleNextClick} />}
-            {step === 2 && <Parking />}
-          </>
-
+              {step === 1 && <Find_1 onNextClick={handleNextClick} />}
+              {step === 2 && <Parking />}
+            </>
           ) : (
             <Find_1 onNextClick={handleNextClick} />
           )}
