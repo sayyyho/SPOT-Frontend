276bc14c3c8a2dfa3e2a47230ef299fffd2fc39a
SEHO PARK <34148750+sayyyho@users.noreply.github.com>
2024-05-11 16:05:09 +0900

    spotUpload.js 업데이트

--- client/src/apis/spotUpload.js
+++ client/src/apis/spotUpload.js
@@ -3,15 +3,16 @@ import {instance} from './axios';
 
 export const spotUpload = async (imageFile) => {
   try {
-    const data = {
+       const data = {
       distance: 1.2,
       time: 10,
     };
+    const json = JSON.stringify(data);
+    const blob = new Blob([json], { type: 'application/json' });
 
     const formData = new FormData();
     formData.append('image', imageFile);
-    formData.append('dto', JSON.stringify(data));
-
+    formData.append('dto', blob);
     const response = await instance.post(`/api/boarding-record`, formData, {
       headers: {
         'Content-Type': 'multipart/form-data',
